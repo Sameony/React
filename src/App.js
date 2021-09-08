c10bd960a07a6460fac1f1efea4792aaba483beb
Sameony <80711125+Sameony@users.noreply.github.com>
2021-09-08 17:40:46 +0530

    EmojiTranslator skeleton

--- src/App.js
+++ src/App.js
@@ -1,19 +1,54 @@
 import "./styles.css";
 import React, { useState } from "react";
 
-var user = "sam";
-var color = "skyBlue";
-
+var emojiDict = {
+  "🦰": "Red hairs",
+  "🤪": "Crazy face",
+  "🐷": "Pig",
+  "🦓": "Zebra",
+  "🦄": "Unicorn",
+  "🐫": "Camel",
+  "🐵": "Monkey",
+  "🐶": "Dog",
+  "😊": "Smiling",
+  "😎": "Handsome",
+  "😥": "Sad",
+  "😪": "Sleepy",
+  "😆": "Funny",
+  "🤨": "Face with raised eyebrows",
+  "😇": "Smiling face with halo",
+  "🤩": "Star-Struck",
+  "😘": "Face Blowing a Kiss",
+  "😗": "Kissing Face",
+  "☺ ️": "Smiling Face",
+  "😚": "Kissing Face with Closed Eyes",
+  "😙": "Kissing Face with Smiling Eyes",
+  "😷": "Face with Medical Mask",
+  "🤒": "Face with Thermometer",
+  "🤕": "Face with Head-Bandage",
+  "🤢": "Nauseated Face",
+  "🤮": "Face Vomiting",
+  "🤧": "Sneezing Face",
+  "🥵": "Hot Face",
+  "🥶": "Cold Face",
+  "🥴": "Woozy Face",
+  "😵": "Dizzy Face",
+  "😵‍💫": "Face with Spiral Eyes",
+  "🤯": " Exploding Head"
+};
 export default function App() {
-  const [word, setWord] = useState("");
-  function changeEventHandler(change) {
-    setWord(change.target.value);
+  const [ans, setAns] = useState("");
+  function ChangeEventHandler(changedInput) {
+    if (changedInput.target.value in emojiDict)
+      setAns(emojiDict[changedInput.target.value]);
+    else setAns("Sorry, This Emoji is not registered in our database.");
   }
+
   return (
-    <div className="App">
-      <h1 style={{ backgroundColor: color }}>Welcome {user}</h1>
-      <input onChange={changeEventHandler}></input>
-      <div>{word}</div>
+    <div>
+      <h1> Check </h1>
+      <input onChange={ChangeEventHandler}></input>
+      <div>{ans}</div>
     </div>
   );
 }
